7e935671ecabf5efd6e5c413d82f99764d55b5d1
dayvster <dayvi.schuster@protonmail.com>
2026-06-08 16:21:33 +0200

    bugfix: explicit linux platform support, else unsupported

--- src/core/platform.zig
+++ src/core/platform.zig
@@ -3,5 +3,6 @@ const builtin = @import("builtin");
 pub const AppReader = switch (builtin.os.tag) {
     .macos => @import("osx/appreader.zig").AppReader,
     .windows => @compileError("Windows support not implemented"),
-    else => @import("appreader.zig").AppReader,
+    .linux => @import("appreader.zig").AppReader,
+    else => @compileError("Unsupported platform"),
 };
